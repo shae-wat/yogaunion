a37faa06d41846f878626e49dcac50bee98dd82d
shaelyn-watson <shaelynjoy@gmail.com>
2016-05-29 11:19:30 -0700

    initial version of about/links page

---
+++ about-links.php
@@ -0,0 +1,87 @@
+<a href="https://twitter.com/YogaUnionCWC" target="_blank"><img class="alignleft no-border wp-image-4450" src="/wp-content/uploads/2012/01/Bird-Twitter-Watercolor-300x218.jpg" alt="Bird-Twitter-Watercolor-300x218" width="134" height="101" />Follow Us On Twitter</a>
+
+<hr size="2" width="100%" />
+
+Brant Kaake is a licensed and bonded general contractor with a diverse skill set. He has been our go-to fix-it guy at the studio for many years now.
+
+503-523-7807
+film43@yahoo.com
+
+<hr />
+
+<a title="The Effect of Yoga on General Health" href="http://yogaunioncwc.com/effect-yoga-general-health/" target="_blank">Vibrant Valley Farm</a> is a CSA (community-supported agriculture). This spring/summer they will provide the Yoga Union community an abundance of fresh vegetables and other yummy, healthy treats at an affordable price.
+
+<hr />
+
+<a href="http://www.facebook.com/PoorBoyPossibilities">Poor Boy Possibilities </a>LLC. has been helping Portland businesses create comprehensive and effective marketing strategies that involve the use of Social Media. A startup company with fresh, out-of-the-box ideas that are grounded in tried-and-true marketing techniques. They use promotional videos, websites, smart phones, location services, as well as manage social presences on Facebook, Twitter, LinkedIn...etc. They provide real-time CRM and monitor your brands online presence and reviews.
+
+Jeremy D. Schaffer
+CEO/Founder
+Poor Boy Possibilities LLC.
+2737 SE 26th Ave.
+Portland OR, 97214
+503-875-6166
+<a href="mailto:PoorBoyPossibilities@gmail.com">PoorBoyPossibilities@gmail.com</a>
+<a title="Poorboy Possibilities" href="http://facebook.com/PoorBoyPossibilities">Facebook.com/PoorBoyPossibilities</a>
+
+<hr size="2" width="100%" />
+
+<a href="http://www.katesmithnd.com/" target="_blank">Kate Smith</a> is a naturopathic doctor who puts heart and soul into healing. Just being in her presence will make you feel better. The work that she does offers positive results without resorting to harsh chemicals or "drugs." Dietary advice, skin issues, colds... Dr.Kate can help.
+<p align="left"><a href="http://www.katesmithnd.com/" target="_blank">Kate Smith, N.D.</a>
+Homeopathic Doctor
+4225 NE Tillamook St.
+Portland, OR 97213
+Phone: 503-443-2154
+fax: 503-217-6242</p>
+
+
+<hr size="2" width="100%" />
+<p align="left"><a href="http://mttaborchiropractic.com/" target="_blank">Mt. Tabor Chiropractic</a> and Yoga Union are neighboring health centers. <a href="http://mttaborchiropractic.com/" target="_blank">Steve Kingston</a> and his wife JoAnn have been our friends, our students, our mentors, and our chiropractors. Go see them for injury treatment, dietary advice, or a smooth adjustment.<a href="http://mttaborchiropractic.com/" target="_blank">
+</a></p>
+<p align="left"><a href="http://mttaborchiropractic.com/" target="_blank">Steve M. Kingston, D.C.</a>
+4351 SE Hawthorne Blvd.
+Portland, OR 97215
+503-236-1528</p>
+
+
+<hr />
+
+<a href="http://chriscalarcoyoga.com/" target="_blank">Chris Calarco</a> is a yoga instructor and student with a deep passion for music and yoga. He believes they are two paths up the same mountain. With a practice rooted in Anusara Yoga and it's Shiva-Shakti Tantric philosophy, Chris teaches and writes in Portland, Oregon.
+
+<hr />
+
+Photographers We've Worked With:
+<ul>
+    <li><a href="http://www.lisaskaff.com/">Lisa Skaff</a> is a gifted photographer and a joy to work with. She operates with a simplicity that makes communication enjoyable. Her work is creative and fun, with a sweet and soft style.
+<a href="http://www.lisaskaff.com/">www.lisaskaff.com</a></li>
+    <li><a href="jaineedial@gmail.com" target="_blank">Jainee Dial</a></li>
+    <li><a>Ben Moon</a></li>
+    <li><a title="Mirifoto photography" href="http://www.mirifoto.com/">Mififoto</a> photography company</li>
+    <li><a href="http://christiancolumbres.com">Christian Columbres</a></li>
+</ul>
+
+<hr size="2" width="100%" />
+
+Our website was built by <a href="http://www.studiok40.com" target="_blank">Gray Ayer</a>, a Portland-based designer and web developer specializing in creating visually pleasing websites that are easy to use by a site's clients and a breeze to maintain by a site's owner. He is also a dedicated yogi.
+
+<a href="http://www.studiok40.com" target="_blank">Gray Ayer</a>
+<a href="http://www.studiok40.com" target="_blank">www.studiok40.com</a>
+503-209-1281
+<a title="Lets get started on your project right now!" href="mailto:gray@studiok40.com?subject=from%20studiok40%20sidebar">gray@studiok40.com</a>
+
+<hr />
+
+Apas House is a cozy bed and breakfast located in relatively close proximity to Yoga Union. Lisa Skaff will go the extra mile to accommodate even the most selective yogi standards with home-grown, sustainably harvested meals and organic bedding for all. If your in need of a place to stay while your in town for a workshop or training, check it out.
+
+Apas House
+<a title="Apas House Bed and Breakfast" href="http://www.apashouse.com/" target="_blank">http://www.apashouse.com/</a>
+apashousebedandbreakfast@gmail.com
+
+<hr />
+
+<a href="http://www.opentograce.com/">Sianna Sherman</a> is an internationally recognized <a href="http://www.anusara.com/">Anusara </a>Yoga teacher who delights in sharing these teachings with poetry, stories, and expressive insight. We hold space for her at the heart of the Yoga Union community as our true teacher, as our beloved friend, and as a source of what seems to us like an endless stream of divine insight. Please take every chance you get to be in the presence of this sweet spirit.
+
+<a title="Sianna Sherman Website" href="http://www.opentograce.com/" target="_blank">www.opentograce.com</a>
+<a href="http://www.opentograce.com/contact.cfm" target="_blank"> Contact Sianna</a>
+
+&nbsp;
