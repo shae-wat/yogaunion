a2028b41465660d3dd1150019578e6104e775176
shaelyn-watson <shaelynjoy@gmail.com>
2016-07-16 21:09:11 -0700

    fix yoga groove links

--- yoga-groove.php
+++ yoga-groove.php
@@ -1,6 +1,6 @@
 <strong>July 23rd</strong> is coming up fast- have you been practicing your dance moves in anticipation of Yoga Union's first ever <strong>Yoga Groove</strong>?
 
-<strong>Chris Calarco</strong> will be leading a class set to the powerful sounds of improvised music by <strong>DJ HyFi</strong> (check him out here: <a title="dj hyfi's website" href="http//www.yogadjhyfi.com" target="_blank">http//www.yogadjhyfi.com</a>)
+<strong>Chris Calarco</strong> will be leading a class set to the powerful sounds of improvised music by <strong>DJ HyFi</strong> (check him out <a href="https://soundcloud.com/hyfi" target="_blank">on soundcloud</a>)
 
 -Ian traves all around the U.S. bringing with him a fusion of sound and music). These jams will match and inspire the energetic aligned flow of the class, allowing us all to "go deep into our hearts" and rock it out while we stretch it out.
 
@@ -8,9 +8,10 @@ After the 2 hour spanda flow (<strong>spanda</strong> is a Sanskrit term for th
 
 It's also a great opportunity to get to know your fellow yogis you see practicing next to you everyday! And for just 20 bucks, can you really ask for anything else?
 
-Check out <a title="The website of the coolest yoga studio in Portland " href="http://yogaunion.com/" target="_blank">http://yogaunion.com</a> for or additional information, or Chris's own website at <a title="Website of Chris Calarco Yoga " href="http://chriscalarcoyoga.com/" target="_blank">http://chriscalarcoyoga.com/</a>
+Check out <a title="The website of the coolest yoga studio in Portland " href="http://www.yogaunioncwc.com/" target="_blank">http://www.yogaunioncwc.com/</a> for or additional information, or Chris's own website at <a title="Website of Chris Calarco Yoga " href="http://chriscalarcoyoga.com/" target="_blank">http://chriscalarcoyoga.com/</a>
 
 <strong>We hope to see you there!</strong>
 
 
-Additional information from:                                              <a title="Spanda Flow Website " href="http://www.spandaflow.com" target="_blank">Http://www.spandaflow.com</a> and <a title="Yoga Journal Website " href="http://www.yogajournal.com" target="_blank">Http://www.yogajournal.com</a>
+Additional information from:
+<a title="Yoga Journal Website " href="http://www.yogajournal.com" target="_blank">http://www.yogajournal.com</a>
