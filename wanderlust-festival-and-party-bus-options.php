7c1920ea77023489dbee7be4ef19bffa0e03779c
shaelyn-watson <shaelynjoy@gmail.com>
2016-07-16 20:41:14 -0700

    inital wanderlust page

---
+++ wanderlust-festival-and-party-bus-options.php
@@ -0,0 +1,23 @@
+<a href="http://yogaunioncwc.com/wp-content/uploads/2011/03/header.jpeg"><img class="aligncenter size-full wp-image-239" title="header" src="http://yogaunioncwc.com/wp-content/uploads/2011/03/header.jpeg" alt="" width="500" height="165" /></a>
+<p style="text-align:center;"><strong>Party Bus to Tahoe</strong></p>
+<p style="text-align:center;"><strong> </strong></p>
+<p style="text-align:center;">Join the Yoga Union on our first ever festival field trip. We'll build the  spirit of community as we travel together aboard a 16 passenger van. <strong>Purchase your wanderlust tickets from Yoga Union and receive a 5% discount</strong>. This is sure to be an epic and unforgettable adventure!</p>
+
+<div style="text-align:center;">
+The bus will depart from the Yoga Union July 27th at 8:00 am, and return by 8:00 pm August 1st.
+
+<strong>Space is extremely limited, so reserve your spot today!
+
+</strong>Reserve your festival and/or party bus tickets by April 30th. You can purchase either of these online!<strong> <a href="http://r20.rs6.net/tn.jsp?llr=suvogrcab&amp;et=1104956663248&amp;s=8052&amp;e=001mmi0S93t7XRRZFPzacVRMJw7O8wzcqEU0hyOZ2GjYWeAjMM-ffzlZwECIy6023R-VI39lZcE6F8Pbk3qvh1oTexwRTzMuyl_ZmKnxYrlNrBADRV9d9qMyZfAHPRQN0HlEs8CEJEO58_TVgs2MKKNpmi9bdJrm88qqXrZLAlSVCs7TKGEoJ4dtCTVaFnp0EvQ" target="_blank">PURCHASE NOW</a></strong></div>
+<a href="http://yogaunioncwc.com/wp-content/uploads/2011/03/532116566tdxqov_fs.jpeg"><img class="aligncenter size-full wp-image-240" title="532116566TdXQOV_fs" src="http://yogaunioncwc.com/wp-content/uploads/2011/03/532116566tdxqov_fs.jpeg" alt="" width="500" height="375" /></a><a href="http://yogaunioncwc.com/wp-content/uploads/2011/03/94.jpeg"><img class="aligncenter size-full wp-image-241" title="94" src="http://yogaunioncwc.com/wp-content/uploads/2011/03/94.jpeg" alt="" width="500" height="116" /></a>
+<p style="text-align:center;"><strong>Wanderlust</strong> is a one-of-a-kind festival bringing together the world's best yoga and wellness teachers and top musical acts and DJs, all in a setting of breathtaking natural beauty.</p>
+<p style="text-align:center;">We're talking about fun in the sun and dancing under the stars. Getting your down dog on at the top of the mountain and sipping poolside cocktails with your crew. Early morning meditations and all-night chakra spinning musical performances. The epic Lake Tahoe Valley in all of its picturesque glory. A place to root down and rise up, to get centered on your yoga mat and get funky on the dance floor - lux rooms, fresh healthy food, and good people.Yoga, music, nature, and a whole lot more. <strong>This is Wanderlust California</strong>.</p>
+<p style="text-align:center;"><strong><a href="http://r20.rs6.net/tn.jsp?llr=suvogrcab&amp;et=1104956663248&amp;s=8052&amp;e=001mmi0S93t7XRRZFPzacVRMJw7O8wzcqEU0hyOZ2GjYWeAjMM-ffzlZwECIy6023R-VI39lZcE6F8Pbk3qvh1oTexwRTzMuyl_ZmKnxYrlNrBADRV9d9qMyZfAHPRQN0HlEs8CEJEO58_TVgs2MKKNpmi9bdJrm88qqXrZLAlSVCs7TKGEoJ4dtCTVaFnp0EvQ" target="_blank">PURCHASE NOW</a></strong></p>
+<p style="text-align:center;"><a href="http://r20.rs6.net/tn.jsp?llr=suvogrcab&amp;et=1104956663248&amp;s=8052&amp;e=001mmi0S93t7XTATrNJ-CirqCI87rOAe-sMwCG7HyLP5AQmRp0zRWVkhxjNQF0d7sjfW2yLFNyDHc9asRtjn7W2pYQ6w16w1UFQqaFe3AssfY77MDYTM6-Xm6YzT1vMXP_hpT1hTbub_P4=" target="_blank">Check out the Wanderlust Website.</a></p>
+<p style="text-align:center;">Contact Natalie Gildersleeve for details or to for more information:</p>
+<p style="text-align:center;"><a href="mailto:nataliegildersleeve@gmail.com" target="_blank">nataliegildersleeve@gmail.com</a> <a href="503-260-4872" target="_blank">503-260-4872</a></p>
+<p style="text-align:center;"><a href="http://yogaunioncwc.com/wp-content/uploads/2011/03/89.jpeg"><img class="aligncenter size-full wp-image-242" title="89" src="http://yogaunioncwc.com/wp-content/uploads/2011/03/89.jpeg" alt="" width="500" height="410" /></a><strong>Thank you for your continued support!</strong>
+
+<strong>Sincerely,</strong>
+
+<strong>Your friends at Yoga Union</strong></p>
