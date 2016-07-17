5280dc8719e7167bbea3af9e5986414f9285b996
shaelyn-watson <shaelynjoy@gmail.com>
2016-07-16 20:59:11 -0700

    initial yoga groove php page

---
+++ yoga-groove.php
@@ -0,0 +1,16 @@
+<strong>July 23rd</strong> is coming up fast- have you been practicing your dance moves in anticipation of Yoga Union's first ever <strong>Yoga Groove</strong>?
+
+<strong>Chris Calarco</strong> will be leading a class set to the powerful sounds of improvised music by <strong>DJ HyFi</strong> (check him out here: <a title="dj hyfi's website" href="http//www.yogadjhyfi.com" target="_blank">http//www.yogadjhyfi.com</a>)
+
+-Ian traves all around the U.S. bringing with him a fusion of sound and music). These jams will match and inspire the energetic aligned flow of the class, allowing us all to "go deep into our hearts" and rock it out while we stretch it out.
+
+After the 2 hour spanda flow (<strong>spanda</strong> is a Sanskrit term for the divine vibration, or, the creative pulse of the universe as it manifests into the dynamism of living form), the fun really begins with a hoppin' <strong>dance party</strong> and <strong>adult beverages</strong> including <strong>organic Sokol Blosser wines </strong>(<a title="Organic Sokol Blosser Wines website" href="http://www.sokolblosser.com/" target="_blank">http://www.sokolblosser.com</a>) and local favorite <strong>beers</strong>.
+
+It's also a great opportunity to get to know your fellow yogis you see practicing next to you everyday! And for just 20 bucks, can you really ask for anything else?
+
+Check out <a title="The website of the coolest yoga studio in Portland " href="http://yogaunion.com/" target="_blank">http://yogaunion.com</a> for or additional information, or Chris's own website at <a title="Website of Chris Calarco Yoga " href="http://chriscalarcoyoga.com/" target="_blank">http://chriscalarcoyoga.com/</a>
+
+<strong>We hope to see you there!</strong>
+
+
+Additional information from:                                              <a title="Spanda Flow Website " href="http://www.spandaflow.com" target="_blank">Http://www.spandaflow.com</a> and <a title="Yoga Journal Website " href="http://www.yogajournal.com" target="_blank">Http://www.yogajournal.com</a>
